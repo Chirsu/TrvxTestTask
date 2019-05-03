7b1c5147354759f8bd0812d0c6cc5d4e9d901d08
Alex Pay <payalexeysfw@gmail.com>
2019-05-03 17:29:37 +0300

    Add model data annotations

--- TrvxTask/TrvxTask.Api/Models/CommentModel.cs
+++ TrvxTask/TrvxTask.Api/Models/CommentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace TrvxTask.Api.Models
     public class CommentModel
     {
         public Guid Id { get; set; }
+
+        [StringLength(500, ErrorMessage = "Text Max Length is 500")]
         public string Text { get; set; }
+
         public Guid PostId { get; set; }
     }
 }
--- TrvxTask/TrvxTask.Api/Models/PostModel.cs
+++ TrvxTask/TrvxTask.Api/Models/PostModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace TrvxTask.Api.Models
     public class PostModel
     {
         public Guid Id { get; set; }
+
+        [StringLength(160, ErrorMessage = "Title Max Length is 160")]
         public string Title { get; set; }
+
+        [StringLength(500, ErrorMessage = "Text Max Length is 500")]
         public string Text { get; set; }
     }
 }
